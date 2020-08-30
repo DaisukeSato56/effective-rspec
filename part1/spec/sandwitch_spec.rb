2d01a183a649f7d8cb6741455cd9534eeb600f7c
DaisukeSato56 <workshift56@gmail.com>
2020-08-30 15:00:36 +0900

    helper method

--- part1/spec/sandwitch_spec.rb
+++ part1/spec/sandwitch_spec.rb
@@ -3,15 +3,17 @@
 Sandwitch = Struct.new(:taste, :toppings)
 
 RSpec.describe 'An ideal sandwitch' do
-  before { @sandwitch = Sandwitch.new('delicious', []) }
+  def sandwitch
+    @sandwitch ||= Sandwitch.new('delicious', [])
+  end
 
   it 'is delicious' do
-    expect(@sandwitch.taste).to eq 'delicious'
+    expect(sandwitch.taste).to eq 'delicious'
   end
 
   it 'lets me add toppings' do
-    @sandwitch.toppings << 'cheese'
+    sandwitch.toppings << 'cheese'
 
-    expect(@sandwitch.toppings).not_to be_empty
+    expect(sandwitch.toppings).not_to be_empty
   end
 end
