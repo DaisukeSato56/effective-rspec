16e6678446fd40db9e4ac50aca2c006f902e7e17
DaisukeSato56 <workshift56@gmail.com>
2020-08-30 14:27:08 +0900

    rspec hooks

--- part1/spec/sandwitch_spec.rb
+++ part1/spec/sandwitch_spec.rb
@@ -3,15 +3,15 @@
 Sandwitch = Struct.new(:taste, :toppings)
 
 RSpec.describe 'An ideal sandwitch' do
+  before { @sandwitch = Sandwitch.new('delicious', []) }
+
   it 'is delicious' do
-    sandwitch = Sandwitch.new('delicious', [])
-    expect(sandwitch.taste).to eq 'delicious'
+    expect(@sandwitch.taste).to eq 'delicious'
   end
 
   it 'lets me add toppings' do
-    sandwitch = Sandwitch.new('delicious', [])
-    sandwitch.toppings << 'cheese'
+    @sandwitch.toppings << 'cheese'
 
-    expect(sandwitch.toppings).not_to be_empty
+    expect(@sandwitch.toppings).not_to be_empty
   end
 end
